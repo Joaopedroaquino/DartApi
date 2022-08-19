6cdd61750278c88c0eab4af7b5231708293e6eb9
joaopedroaquino <aqerhack@gmail.com>
2022-08-19 15:19:28 -0300

    instanciando o modulo

--- lib/backend.dart
+++ lib/backend.dart
@@ -3,6 +3,6 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
 Future<Handler> startShelfModular() async {
-  final handler = Modular(module: (AppModule()));
+  final handler = Modular(module: AppModule(), middlewares: [logRequests()]);
   return handler;
 }
