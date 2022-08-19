ea20a4afc52eec777e99247c3004b001e04658d2
joaopedroaquino <aqerhack@gmail.com>
2022-08-19 14:25:49 -0300

    adicionando o handler

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
@@ -9,4 +11,9 @@ Future<void> main(List<String> arguments) async {
   }, '0.0.0.0', 4466);
 
   print('Online - ${server.address.address}:${server.port} ');
+}
+
+FutureOr<Response> _handler(Request request) {
+  print(request);
+  return Response(200, body: 'corpo');
 }
